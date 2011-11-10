888b0fdd49f6890dda2341e41535986d73a8738c
Tom Wahlin <tom.wahlin@gmail.com>
2011-11-10 07:48:54 -0600

    Current images from v3 added. SCSS integration. Beginning styling.

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title></title>
+
+    <link rel="stylesheet" href="public/styles/style.css" type="text/css" />
+    <link rel="Shortcut Icon" href="public/images/favicon.ico" type="image/x-icon" />
+
+    <script src="public/scripts/jquery-1.7.min.js" type="text/javascript" charset="utf-8"></script>
+    <script src="public/scripts/jquery.countdown.min.js" type="text/javascript" charset="utf-8"></script>    
+    <script src="public/scripts/raphael-min.js" type="text/javascript" charset="utf-8"></script>  
+    <script src="public/scripts/scripts.js" type="text/javascript" charset="utf-8"></script>
+    <script>
+    
+    </script>	
+</head>
+
+<body>
+<div class="wrap">
+  <div class="v2"><a href="http://v2.tomwahlin.com">View Version Two</a></div>
+  <div class="experience_container">
+    <div class="logo_wrap">
+      <div class="logo_inner_wrap">
+        <h1><a href="http://tomwahlin.com"><span>Creative Thinker &amp; Problem Solver&mdash;Tom Wahlin</span></a></h1>
+      </div>
+    </div>    
+    <div class="pane_wrapper">
+      <div id="pane"></div>
+    </div>
+    <div id="timer"><noscript>Version 3 of <a href="http://tomwahlin.com">Tomwahlin.com</a> will launch on 12.11.2011</noscript></div>
+    <ul class="links_social">
+       <li class="dribbble"><a rel="external" title="Visit Tom Wahlin&rsquo;s Dribbble" href="http://dribbble.com/twahlin"><em class="button">Dribbble </em><span>View Shots on Dribbble</span></a></li>
+       <li class="facebook"><a rel="external" title="Visit Tom Wahlin&rsquo;s Facebook" href="http://www.facebook.com/twahlin"><em class="button">Facebook </em><span>Friend on Facebook</span></a></li>
+       <li class="flickr"><a rel="external" title="Visit Tom Wahlin&rsquo;s Flickr" href="http://www.flickr.com/photos/twahlin"><em class="button">Flickr </em><span>View Photos on Flickr</span></a></li>
+       <li class="linkedin"><a rel="external" title="Visit Tom Wahlin&rsquo;s Linkedin" href="http://www.linkedin.com/in/tomwahlin"><em class="button">Linkedin </em><span>Connect <strong class="small_caps">via</strong> Linkedin</span></a></li>
+       <li class="last_fm"><a rel="external" title="Visit Tom Wahlin&rsquo;s Last.fm" href="http://www.last.fm/user/twahlin"><em class="button">Last.fm </em><span>Listen on Last.fm</span></a></li>
+       <li class="twitter"><a rel="external" title="Visit Tom Wahlin&rsquo;s Twitter" href="http://twitter.com/twahlin"><em class="button">Twitter </em><span>Follow on Twitter</span></a></li>
+     </ul>    
+  </div>
+</div>
+</body>
+</html>
