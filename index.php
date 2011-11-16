c5c8efe6e4b6816f4c9dbfbcdf8527f75284974d
Tom Wahlin <tom.wahlin@gmail.com>
2011-11-16 08:49:01 -0600

    Page styling. Media queries for different widths.

--- index.php
+++ index.php
@@ -1,42 +1,59 @@
 <!DOCTYPE html>
+<!--[if IE 6]><meta HTTP-EQUIV="REFRESH" content="0; url=http://v2.tomwahlin.com"><![endif]-->
+<!--[if IEMobile 7 ]><html class="no-js iem7" manifest="default.appcache?v=1"><![endif]-->
+<!--[if lt IE 7 ]><html class="no-js ie6" lang="en"><![endif]-->
+<!--[if IE 7 ]><html class="no-js ie7" lang="en"><![endif]-->
+<!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" manifest="default.appcache?v=1" lang="en"><!--<![endif]-->
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <title></title>
+  <meta charset="utf-8">
+  <title>The Portfolio of Tom Wahlin</title>
+  <meta name="description" content="Splash page for the launch of tomwhalin.com version 3">
+  <meta name="author" content="Tom Wahlin">
 
-    <link rel="stylesheet" href="public/styles/style.css" type="text/css" />
-    <link rel="Shortcut Icon" href="public/images/favicon.ico" type="image/x-icon" />
+  <!-- http://t.co/dKP3o1e -->
+  <meta name="HandheldFriendly" content="True">
+  <meta name="MobileOptimized" content="320">
+  <meta name="viewport" content="width=device-width, target-densitydpi=160dpi, initial-scale=1">
+  
+  <link rel="stylesheet" href="public/styles/style.css" type="text/css" />
+  <link rel="Shortcut Icon" href="public/images/favicon.ico" type="image/x-icon" />
 
-    <script src="public/scripts/jquery-1.7.min.js" type="text/javascript" charset="utf-8"></script>
-    <script src="public/scripts/jquery.countdown.min.js" type="text/javascript" charset="utf-8"></script>    
-    <script src="public/scripts/raphael-min.js" type="text/javascript" charset="utf-8"></script>  
-    <script src="public/scripts/scripts.js" type="text/javascript" charset="utf-8"></script>
-    <script>
-    
-    </script>	
+  <script src="public/scripts/jquery-1.7.min.js" type="text/javascript" charset="utf-8"></script>
+  <script type="text/javascript" src="http://use.typekit.com/zbv5ytb.js"></script>
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+  <script src="public/scripts/jquery.countdown.min.js" type="text/javascript" charset="utf-8"></script>    
+  <script src="public/scripts/raphael-min.js" type="text/javascript" charset="utf-8"></script>  
+  <script src="public/scripts/plugins.js" type="text/javascript" charset="utf-8"></script>  
+  <script src="public/scripts/scripts.js" type="text/javascript" charset="utf-8"></script>	
 </head>
 
 <body>
-<div class="wrap">
-  <div class="v2"><a href="http://v2.tomwahlin.com">View Version Two</a></div>
-  <div class="experience_container">
-    <div class="logo_wrap">
-      <div class="logo_inner_wrap">
-        <h1><a href="http://tomwahlin.com"><span>Creative Thinker &amp; Problem Solver&mdash;Tom Wahlin</span></a></h1>
+  <div class="wrap">
+    
+      <div class="v2"><a href="http://v2.tomwahlin.com">&laquo; View tomwahlin.com Version Two</a></div>
+      <div class="wrap_inner">
+      <div class="experience_container">
+        <div class="logo_wrap">
+          <div class="logo_inner_wrap">
+            <h1><a href="http://tomwahlin.com"><span>Creative Thinker &amp; Problem Solver&mdash;Tom Wahlin</span></a></h1>
+          </div>
+        </div>    
+        <div class="pane_wrapper">
+          <div id="pane" class="pane"></div>
+        </div>
+        <h2>Version 3 Coming Soon</h2>
+        <div id="timer" class="timer"><noscript>Version 3 of <a href="http://tomwahlin.com">Tomwahlin.com</a> will launch on 12.11.2011</noscript></div>
+        <ul class="links_social">
+          <li class="dribbble"><a rel="external" title="Visit Tom Wahlin&rsquo;s Dribbble" href="http://dribbble.com/twahlin"><em class="button">Dribbble </em><span>View Shots on Dribbble</span></a></li>
+          <li class="facebook"><a rel="external" title="Visit Tom Wahlin&rsquo;s Facebook" href="http://www.facebook.com/twahlin"><em class="button">Facebook </em><span>Friend on Facebook</span></a></li>
+          <li class="flickr"><a rel="external" title="Visit Tom Wahlin&rsquo;s Flickr" href="http://www.flickr.com/photos/twahlin"><em class="button">Flickr </em><span>View Photos on Flickr</span></a></li>
+          <li class="linkedin"><a rel="external" title="Visit Tom Wahlin&rsquo;s Linkedin" href="http://www.linkedin.com/in/tomwahlin"><em class="button">Linkedin </em><span>Connect <strong class="small_caps">via</strong> Linkedin</span></a></li>
+          <li class="last_fm"><a rel="external" title="Visit Tom Wahlin&rsquo;s Last.fm" href="http://www.last.fm/user/twahlin"><em class="button">Last.fm </em><span>Listen on Last.fm</span></a></li>
+          <li class="twitter"><a rel="external" title="Visit Tom Wahlin&rsquo;s Twitter" href="http://twitter.com/twahlin"><em class="button">Twitter </em><span>Follow on Twitter</span></a></li>
+        </ul>    
       </div>
-    </div>    
-    <div class="pane_wrapper">
-      <div id="pane" class="pane"></div>
     </div>
-    <div id="timer" class="timer"><noscript>Version 3 of <a href="http://tomwahlin.com">Tomwahlin.com</a> will launch on 12.11.2011</noscript></div>
-    <ul class="links_social">
-       <li class="dribbble"><a rel="external" title="Visit Tom Wahlin&rsquo;s Dribbble" href="http://dribbble.com/twahlin"><em class="button">Dribbble </em><span>View Shots on Dribbble</span></a></li>
-       <li class="facebook"><a rel="external" title="Visit Tom Wahlin&rsquo;s Facebook" href="http://www.facebook.com/twahlin"><em class="button">Facebook </em><span>Friend on Facebook</span></a></li>
-       <li class="flickr"><a rel="external" title="Visit Tom Wahlin&rsquo;s Flickr" href="http://www.flickr.com/photos/twahlin"><em class="button">Flickr </em><span>View Photos on Flickr</span></a></li>
-       <li class="linkedin"><a rel="external" title="Visit Tom Wahlin&rsquo;s Linkedin" href="http://www.linkedin.com/in/tomwahlin"><em class="button">Linkedin </em><span>Connect <strong class="small_caps">via</strong> Linkedin</span></a></li>
-       <li class="last_fm"><a rel="external" title="Visit Tom Wahlin&rsquo;s Last.fm" href="http://www.last.fm/user/twahlin"><em class="button">Last.fm </em><span>Listen on Last.fm</span></a></li>
-       <li class="twitter"><a rel="external" title="Visit Tom Wahlin&rsquo;s Twitter" href="http://twitter.com/twahlin"><em class="button">Twitter </em><span>Follow on Twitter</span></a></li>
-     </ul>    
   </div>
-</div>
 </body>
 </html>
